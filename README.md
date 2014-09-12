e8640de60ab7b33859cb9c7f21a8c26980790d3e
Daniel Qiu <daniel@danielqiu.com>
2014-09-11 21:23:06 -0700

    fixes to work with Meteor 0.9.x

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 meteor-iconv
 =================
 
-Meteor smart package for [node-iconv](https://github.com/bnoordhuis/node-iconv).
+Meteor package for [node-iconv](https://github.com/bnoordhuis/node-iconv).
+
+#Usage
+
+Install package from Meteor:
+```
+meteor add mrt:iconv 
+
+```
--- package.js
+++ package.js
@@ -1,11 +1,18 @@
 Package.describe({
-	summary: "Text recoding in JavaScript in meteor "
+	name: "mrt:iconv",
+	summary: "Text recoding in JavaScript in meteor ",
+	author: "Daniel Qiu",
+	version: "2.0.7",
+	git: "https://github.com/danielqiu/meteor-iconv.git"
 });
 
 Npm.depends({iconv: "2.0.7"});
 
 Package.on_use(function(api){
+	api.versionsFrom('METEOR@0.9.1.1');
+
 	api.add_files(['iconv.js'], 'server', 'client');
+	
 	if(api.export)
 		api.export('Iconv');
 });
