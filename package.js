31e48ff7353fb9f1f1f7d3d6b90119220395858c
Daniel Qiu <daniel.qiuliang@gmail.com>
2014-11-17 00:03:09 -0800

    Update package.js
    bump to 2.1.4 and works with Meteor 1.0.0

--- package.js
+++ package.js
@@ -2,14 +2,14 @@ Package.describe({
 	name: "mrt:iconv",
 	summary: "Text recoding in JavaScript in meteor ",
 	author: "Daniel Qiu",
-	version: "2.0.7",
+	version: "2.1.4",
 	git: "https://github.com/danielqiu/meteor-iconv.git"
 });
 
-Npm.depends({iconv: "2.0.7"});
+Npm.depends({iconv: "2.1.4"});
 
 Package.on_use(function(api){
-	api.versionsFrom('METEOR@0.9.1.1');
+	api.versionsFrom('METEOR@1.0.0');
 
 	api.add_files(['iconv.js'], 'server', 'client');
 	
